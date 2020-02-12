e8c7c6aa442137a11ff3c08f26d10f551ce4df19
Patrick Ting <pcting@gmail.com>
2020-02-12 04:22:04 -0800

    Add example-class-demo example

--- magefile.go
+++ magefile.go
@@ -6,7 +6,10 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
+	"runtime"
 
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
@@ -29,12 +32,6 @@ func envWithPlatform(platform BuildPlatform) map[string]string {
 	}
 }
 
-func Generate() error {
-	mg.Deps(Clean)
-
-	return sh.RunV(cForGoCmd, "-ccdefs", "-ccincl", "-debug", "godot-go.yaml")
-}
-
 // Remove the temporarily generated files from Release.
 func Clean() error {
 	files := []string{"cgo_helpers.go", "cgo_helpers.c", "cgo_helpers.h", "const.go", "godot.go", "types.go"}
@@ -47,6 +44,46 @@ func Clean() error {
 	}
 
 	return nil
+}
+
+// Generate c-for-go go files.
+func Generate() error {
+	mg.Deps(Clean)
+
+	return sh.RunV(cForGoCmd, "-ccdefs", "-ccincl", "-debug", "godot-go.yaml")
+}
+
+// Build Examples
+func BuildExamples() error {
+	mg.Deps(Generate)
+
+	appPath := filepath.Join("examples", "example-class-demo")
+
+	return buildExample(
+		appPath,
+		BuildPlatform{
+			OS:   runtime.GOOS,
+			Arch: runtime.GOARCH,
+		},
+	)
+}
+
+func buildExample(appPath string, platform BuildPlatform) error {
+	gocmd := mg.GoCmd()
+	if _, err := os.Stat("dist"); os.IsNotExist(err) {
+		if err := os.Mkdir("dist", 0755); err != nil {
+			return err
+		}
+	}
+	return sh.RunWith(envWithPlatform(platform), gocmd, "build", "-x", "-work",
+		"-buildmode=c-shared",
+		"-o", filepath.Join("dist", platform.exampleBinaryName(appPath)),
+		filepath.Join(appPath, "main.go"),
+	)
+}
+
+func (p *BuildPlatform) exampleBinaryName(appPath string) string {
+	return fmt.Sprintf("libgodotgo-%s-%s-%s.so", filepath.Base(appPath), p.OS, p.Arch)
 }
 
 // tag returns the git tag for the current branch or "" if none.

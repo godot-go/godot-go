55d530d69834658983709a4f1903ce6de2a4167d
Patrick Ting <pcting@gmail.com>
2020-07-26 22:05:03 -0700

    fix(build): package release

--- package-release.sh
+++ package-release.sh
@@ -18,6 +18,15 @@ cd godot_headers
 git archive 3.2 | tar -x -C ../dist/godot_headers
 cd ../dist
 git init .
+rm .gitignore
+cp ../pkg/gdnative/*.wrappergen.h pkg/gdnative/
+cp ../pkg/gdnative/*.wrappergen.c pkg/gdnative/
+cp ../pkg/gdnative/*.typegen.go pkg/gdnative/
+cp ../pkg/gdnative/*.classgen.go pkg/gdnative/
+git add -f pkg/gdnative/*.wrappergen.h
+git add -f pkg/gdnative/*.wrappergen.c
+git add -f pkg/gdnative/*.typegen.go
+git add -f pkg/gdnative/*.classgen.go
 git add .
 git commit -m "release $1"
 git tag $1 -f

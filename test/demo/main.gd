a990b9956f0b467397326f8fbd828cbc743943c5
Patrick Ting <pcting@gmail.com>
2022-09-28 02:44:01 -0700

    tweak

--- test/demo/main.gd
+++ test/demo/main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-# var exa = preload("example.gdextension")
+var exa = preload("example.gdextension")
 
 func _ready():
 	# prints("is_library_open =", exa.is_library_open(), "min level =", exa.get_minimum_library_initialization_level())

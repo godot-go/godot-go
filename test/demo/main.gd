4183aa6c2030eade79b02cc6c73cf1ca6d499ffa
Patrick Ting <pcting@gmail.com>
2022-09-28 01:19:27 -0700

    test without static functions

--- test/demo/main.gd
+++ test/demo/main.gd
@@ -12,18 +12,18 @@ func _ready():
 	prints("")
 
 	# Call static methods.
-	prints("Static method calls")
-	prints("  static (109)", Example.test_static(9, 100));
-	Example.test_static2();
+	# prints("Static method calls")
+	# prints("  static (109)", Example.test_static(9, 100));
+	# Example.test_static2();
 
-	# Call methods.
-	prints("Instance method calls")
-	$Example.simple_func()
-	($Example as Example).simple_const_func(123) # Force use of ptrcall
-	prints("  returned", $Example.return_something("some string", 7.0/6, 7.0/6 * 1000, 2147483647, -127, -32768, 2147483647, 9223372036854775807))
-	prints("  returned const", $Example.return_something_const())
-	# prints("  returned ref", $Example.return_extended_ref())
-	prints("  returned ", $Example.get_v4())
+	# # Call methods.
+	# prints("Instance method calls")
+	# $Example.simple_func()
+	# ($Example as Example).simple_const_func(123) # Force use of ptrcall
+	# prints("  returned", $Example.return_something("some string", 7.0/6, 7.0/6 * 1000, 2147483647, -127, -32768, 2147483647, 9223372036854775807))
+	# prints("  returned const", $Example.return_something_const())
+	# # prints("  returned ref", $Example.return_extended_ref())
+	# prints("  returned ", $Example.get_v4())
 
 	# prints("VarArg method calls")
 	# var ref = ExampleRef.new()

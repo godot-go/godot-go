b6bcfca4a381a86d592257a296379701331c3569
Patrick Ting <pcting@gmail.com>
2022-09-30 00:47:14 -0700

    tweak

--- pkg/log/stacktrace.c
+++ pkg/log/stacktrace.c
@@ -4,6 +4,72 @@ bool enablePrintStacktrace = false;
 
 void printStacktrace() {
     if (enablePrintStacktrace) {
+    #ifdef _WIN64
+    BOOL                result;
+    HANDLE              process;
+    HANDLE              thread;
+    CONTEXT             context;
+    STACKFRAME64        stack;
+    ULONG               frame;
+    IMAGEHLP_SYMBOL64   symbol;
+    DWORD64             displacement;
+    char name[ 256 ];
+
+    RtlCaptureContext( &context );
+    memset( &stack, 0, sizeof( STACKFRAME64 ) );
+
+    process                = GetCurrentProcess();
+    thread                 = GetCurrentThread();
+    displacement           = 0;
+    stack.AddrPC.Offset    = context.Eip;
+    stack.AddrPC.Mode      = AddrModeFlat;
+    stack.AddrStack.Offset = context.Esp;
+    stack.AddrStack.Mode   = AddrModeFlat;
+    stack.AddrFrame.Offset = context.Ebp;
+    stack.AddrFrame.Mode   = AddrModeFlat;
+
+    for( frame = 0; ; frame++ )
+    {
+        result = StackWalk64
+        (
+            IMAGE_FILE_MACHINE_I386,
+            process,
+            thread,
+            &stack,
+            &context,
+            NULL,
+            SymFunctionTableAccess64,
+            SymGetModuleBase64,
+            NULL
+        );
+
+        symbol.SizeOfStruct  = sizeof( IMAGEHLP_SYMBOL64 );
+        symbol.MaxNameLength = 255;
+
+        SymGetSymFromAddr64( process, ( ULONG64 )stack.AddrPC.Offset, &displacement, &symbol );
+        UnDecorateSymbolName( symbol.Name, ( PSTR )name, 256, UNDNAME_COMPLETE );
+
+        printf
+        (
+            "Frame %lu:\n"
+            "    Symbol name:    %s\n"
+            "    PC address:     0x%08LX\n"
+            "    Stack address:  0x%08LX\n"
+            "    Frame address:  0x%08LX\n"
+            "\n",
+            frame,
+            symbol.Name,
+            ( ULONG64 )stack.AddrPC.Offset,
+            ( ULONG64 )stack.AddrStack.Offset,
+            ( ULONG64 )stack.AddrFrame.Offset
+        );
+
+        if( !result )
+        {
+            break;
+        }
+    }
+    #else
         printf("=[ start backtrace ]=============\n\n");
         void* callstack[128];
         int i, frames = backtrace(callstack, 128);
@@ -13,5 +79,6 @@ void printStacktrace() {
         }
         free(strs);
         printf("=[ end backtrace ]===============\n\n");
+    #endif
     }
 }
--- pkg/log/stacktrace.h
+++ pkg/log/stacktrace.h
@@ -1,7 +1,12 @@
 #ifndef CGO_GODOT_GO_STACKTRACE_H
 #define CGO_GODOT_GO_STACKTRACE_H
 
+#ifdef _WIN64
+#include <processthreadsapi.h>
+#include <dbghelp.h>
+#else
 #include <execinfo.h>
+#endif
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>

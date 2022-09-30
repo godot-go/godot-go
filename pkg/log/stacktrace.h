82bcf195497ce52cb87f30353cdee6b233b16d06
Patrick Ting <pcting@gmail.com>
2022-09-30 02:18:24 -0700

    tweak

--- pkg/log/stacktrace.h
+++ pkg/log/stacktrace.h
@@ -1,7 +1,7 @@
 #ifndef CGO_GODOT_GO_STACKTRACE_H
 #define CGO_GODOT_GO_STACKTRACE_H
 
-#ifdef defined(__MINGW32__) || defined(__MINGW64__)
+#ifdef defined _WIN32 || defined _WIN64 || defined __MINGW32__ || defined __MINGW64__ || defined __CYGWIN__
 #include <windows.h>
 #include <dbghelp.h>
 #else

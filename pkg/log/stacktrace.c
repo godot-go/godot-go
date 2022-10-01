0478ebf01fd040ea9efa4b3a3ae999d070dca39a
Patrick Ting <pcting@gmail.com>
2022-09-30 18:44:01 -0700

    tweak

--- pkg/log/stacktrace.c
+++ pkg/log/stacktrace.c
@@ -4,73 +4,73 @@ bool enablePrintStacktrace = false;
 
 void printStacktrace() {
     if (enablePrintStacktrace) {
-    #ifdef __MINGW32__ || __MINGW64__
-        HANDLE process = GetCurrentProcess();
-        HANDLE thread = GetCurrentThread();
+    #ifdef _WIN32 || _WIN64 || WIN32 || WIN64
+        // HANDLE process = GetCurrentProcess();
+        // HANDLE thread = GetCurrentThread();
 
-        CONTEXT context;
-        memset(&context, 0, sizeof(CONTEXT));
-        context.ContextFlags = CONTEXT_FULL;
-        RtlCaptureContext(&context);
+        // CONTEXT context;
+        // memset(&context, 0, sizeof(CONTEXT));
+        // context.ContextFlags = CONTEXT_FULL;
+        // RtlCaptureContext(&context);
 
-        SymInitialize(process, NULL, TRUE);
+        // SymInitialize(process, NULL, TRUE);
 
-        DWORD image;
-        STACKFRAME64 stackframe;
-        ZeroMemory(&stackframe, sizeof(STACKFRAME64));
+        // DWORD image;
+        // STACKFRAME64 stackframe;
+        // ZeroMemory(&stackframe, sizeof(STACKFRAME64));
 
-        #ifdef _M_IX86
-        image = IMAGE_FILE_MACHINE_I386;
-        stackframe.AddrPC.Offset = context.Eip;
-        stackframe.AddrPC.Mode = AddrModeFlat;
-        stackframe.AddrFrame.Offset = context.Ebp;
-        stackframe.AddrFrame.Mode = AddrModeFlat;
-        stackframe.AddrStack.Offset = context.Esp;
-        stackframe.AddrStack.Mode = AddrModeFlat;
-        #elif _M_X64
-        image = IMAGE_FILE_MACHINE_AMD64;
-        stackframe.AddrPC.Offset = context.Rip;
-        stackframe.AddrPC.Mode = AddrModeFlat;
-        stackframe.AddrFrame.Offset = context.Rsp;
-        stackframe.AddrFrame.Mode = AddrModeFlat;
-        stackframe.AddrStack.Offset = context.Rsp;
-        stackframe.AddrStack.Mode = AddrModeFlat;
-        #elif _M_IA64
-        image = IMAGE_FILE_MACHINE_IA64;
-        stackframe.AddrPC.Offset = context.StIIP;
-        stackframe.AddrPC.Mode = AddrModeFlat;
-        stackframe.AddrFrame.Offset = context.IntSp;
-        stackframe.AddrFrame.Mode = AddrModeFlat;
-        stackframe.AddrBStore.Offset = context.RsBSP;
-        stackframe.AddrBStore.Mode = AddrModeFlat;
-        stackframe.AddrStack.Offset = context.IntSp;
-        stackframe.AddrStack.Mode = AddrModeFlat;
-        #endif
+        // #ifdef _M_IX86
+        // image = IMAGE_FILE_MACHINE_I386;
+        // stackframe.AddrPC.Offset = context.Eip;
+        // stackframe.AddrPC.Mode = AddrModeFlat;
+        // stackframe.AddrFrame.Offset = context.Ebp;
+        // stackframe.AddrFrame.Mode = AddrModeFlat;
+        // stackframe.AddrStack.Offset = context.Esp;
+        // stackframe.AddrStack.Mode = AddrModeFlat;
+        // #elif _M_X64
+        // image = IMAGE_FILE_MACHINE_AMD64;
+        // stackframe.AddrPC.Offset = context.Rip;
+        // stackframe.AddrPC.Mode = AddrModeFlat;
+        // stackframe.AddrFrame.Offset = context.Rsp;
+        // stackframe.AddrFrame.Mode = AddrModeFlat;
+        // stackframe.AddrStack.Offset = context.Rsp;
+        // stackframe.AddrStack.Mode = AddrModeFlat;
+        // #elif _M_IA64
+        // image = IMAGE_FILE_MACHINE_IA64;
+        // stackframe.AddrPC.Offset = context.StIIP;
+        // stackframe.AddrPC.Mode = AddrModeFlat;
+        // stackframe.AddrFrame.Offset = context.IntSp;
+        // stackframe.AddrFrame.Mode = AddrModeFlat;
+        // stackframe.AddrBStore.Offset = context.RsBSP;
+        // stackframe.AddrBStore.Mode = AddrModeFlat;
+        // stackframe.AddrStack.Offset = context.IntSp;
+        // stackframe.AddrStack.Mode = AddrModeFlat;
+        // #endif
 
-        for (size_t i = 0; i < 25; i++) {
+        // for (size_t i = 0; i < 25; i++) {
 
-            BOOL result = StackWalk64(
-            image, process, thread,
-            &stackframe, &context, NULL,
-            SymFunctionTableAccess64, SymGetModuleBase64, NULL);
+        //     BOOL result = StackWalk64(
+        //     image, process, thread,
+        //     &stackframe, &context, NULL,
+        //     SymFunctionTableAccess64, SymGetModuleBase64, NULL);
 
-            if (!result) { break; }
+        //     if (!result) { break; }
 
-            char buffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME * sizeof(TCHAR)];
-            PSYMBOL_INFO symbol = (PSYMBOL_INFO)buffer;
-            symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
-            symbol->MaxNameLen = MAX_SYM_NAME;
+        //     char buffer[sizeof(SYMBOL_INFO) + MAX_SYM_NAME * sizeof(TCHAR)];
+        //     PSYMBOL_INFO symbol = (PSYMBOL_INFO)buffer;
+        //     symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+        //     symbol->MaxNameLen = MAX_SYM_NAME;
 
-            DWORD64 displacement = 0;
-            if (SymFromAddr(process, stackframe.AddrPC.Offset, &displacement, symbol)) {
-            printf("[%i] %s\n", i, symbol->Name);
-            } else {
-            printf("[%i] ???\n", i);
-            }
+        //     DWORD64 displacement = 0;
+        //     if (SymFromAddr(process, stackframe.AddrPC.Offset, &displacement, symbol)) {
+        //     printf("[%i] %s\n", i, symbol->Name);
+        //     } else {
+        //     printf("[%i] ???\n", i);
+        //     }
 
-        }
+        // }
 
-        SymCleanup(process);
+        // SymCleanup(process);
     #else
         printf("=[ start backtrace ]=============\n\n");
         void* callstack[128];
--- pkg/log/stacktrace.h
+++ pkg/log/stacktrace.h
@@ -1,12 +1,13 @@
 #ifndef CGO_GODOT_GO_STACKTRACE_H
 #define CGO_GODOT_GO_STACKTRACE_H
 
-#ifdef defined(_WIN32) || defined(_WIN64) || defined(__MINGW32__) || defined(__MINGW64__) || defined(__CYGWIN__)
+#ifdef _WIN32 || _WIN64
 #include <windows.h>
 #include <dbghelp.h>
 #else
 #include <execinfo.h>
 #endif
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>

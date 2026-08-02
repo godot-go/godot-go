godot-go/godot-go

commit b4a0880cd739a1a70ad16ead41e7fe6be98aefbf

fix: orphan StringName 'Node' from classdb method bind lookups
- Cache className StringNames per class in generated method bindings
  to avoid creating multiple Godot internal references for the same class name
- Add DestroyAllCachedClassNames called during extension deinitialization
- Remove unused StringNameCopyConstructor/NodePathCopyConstructor helpers
- Revert variant_refect_value.go to original byte-copy encoder
- Fix NewGDExtensionPropertyInfoFromNames argument count
- Add gdclassimpl dot-import to test/pkg/lib.go

pkg/core/variant_refect_value.go
@@ -105,11 +105,9 @@ func GDExtensionTypePtrFromReflectValue(value reflect.Value, rOut GDExtensionUni
 		case Color:
 			ColorEncoder.EncodeTypePtrArg(inst, rOut)
 		case StringName:
-			StringNameCopyConstructor(rOut, inst.NativeConstPtr())
-			inst.Destroy()
+			StringNameEncoder.EncodeTypePtrArg(inst, rOut)
 		case NodePath:
-			NodePathCopyConstructor(rOut, inst.NativeConstPtr())
-			inst.Destroy()
+			NodePathEncoder.EncodeTypePtrArg(inst, rOut)
 		case RID:
 			RIDEncoder.EncodeTypePtrArg(inst, rOut)
 		case Callable:
@@ -193,11 +191,9 @@ func GDExtensionTypePtrFromReflectValue(value reflect.Value, rOut GDExtensionUni
 		case Color:
 			ColorEncoder.EncodeTypePtrArg(inst, rOut)
 		case StringName:
-			StringNameCopyConstructor(rOut, inst.NativeConstPtr())
-			inst.Destroy()
+			StringNameEncoder.EncodeTypePtrArg(inst, rOut)
 		case NodePath:
-			NodePathCopyConstructor(rOut, inst.NativeConstPtr())
-			inst.Destroy()
+			NodePathEncoder.EncodeTypePtrArg(inst, rOut)
 		case RID:
 			RIDEncoder.EncodeTypePtrArg(inst, rOut)
 		case Callable:
@@ -336,10 +332,8 @@ func GDExtensionVariantPtrFromReflectValue(value reflect.Value, rOut GDExtension
 			ColorEncoder.EncodeVariantPtrArg(inst, rOut)
 		case StringName:
 			StringNameEncoder.EncodeVariantPtrArg(inst, rOut)
-			inst.Destroy()
 		case NodePath:
 			NodePathEncoder.EncodeVariantPtrArg(inst, rOut)
-			inst.Destroy()
 		case RID:
 			RIDEncoder.EncodeVariantPtrArg(inst, rOut)
 		case Callable:

cmd/generate/gdclassimpl/classes.go.tmpl
@@ -13,6 +13,7 @@ package gdclassimpl
 //revive:disable
 
 import (
+	"sync"
 	"unsafe"
 
 	. "github.com/godot-go/godot-go/pkg/builtin"
@@ -21,6 +22,31 @@ import (
 	"github.com/godot-go/godot-go/pkg/log"
 	"go.uber.org/zap"
 )
+
+var classNameCache sync.Map
+var classNameCacheMu sync.Mutex
+
+func getCachedClassName(name string) StringName {
+	if v, ok := classNameCache.Load(name); ok {
+		return v.(StringName)
+	}
+	classNameCacheMu.Lock()
+	defer classNameCacheMu.Unlock()
+	if v, ok := classNameCache.Load(name); ok {
+		return v.(StringName)
+	}
+	sn := NewStringNameWithLatin1Chars(name)
+	classNameCache.Store(name, sn)
+	return sn
+}
+
+func DestroyAllCachedClassNames() {
+	classNameCache.Range(func(key, value interface{}) bool {
+		sn := value.(StringName)
+		sn.Destroy()
+		return true
+	})
+}
 
 // classes
 {{ range $i, $c := $view.Classes -}}
@@ -81,8 +107,7 @@ func (cx *{{ goClassStructName $c.Name }}) {{ goMethodName $m.Name }}(
 {{- if $m.IsVararg }}varargs ...Variant,{{ end -}}
 ) {{ if $view.IsRefcountedClassName $fnReturnType }}Ref{{ $fnReturnType }}{{ else }}{{ $fnReturnType }}{{ end }} {
     {{/* TODO: refactor for static instantiation */}}
-	className := NewStringNameWithLatin1Chars("{{ $c.Name }}")
-	defer className.Destroy()
+	className := getCachedClassName("{{ $c.Name }}")
 	methodName := NewStringNameWithLatin1Chars("{{ $m.Name }}")
 	defer methodName.Destroy()
 	fn := CallFunc_GDExtensionInterfaceClassdbGetMethodBind(

pkg/builtin/char_string.go
@@ -96,16 +96,6 @@ func GDExtensionStringPtrWithLatin1Chars(ptr GDExtensionStringPtr, content strin
 	CallFunc_GDExtensionInterfaceStringNewWithLatin1Chars((GDExtensionUninitializedStringPtr)(ptr), content)
 }
 
-func StringNameCopyConstructor(out GDExtensionUninitializedTypePtr, src GDExtensionConstTypePtr) {
-	pnr.Pin(unsafe.Pointer(src))
-	CallBuiltinConstructor(globalStringNameMethodBindings.constructor_1, out, src)
-}
-
-func NodePathCopyConstructor(out GDExtensionUninitializedTypePtr, src GDExtensionConstTypePtr) {
-	pnr.Pin(unsafe.Pointer(src))
-	CallBuiltinConstructor(globalNodePathMethodBindings.constructor_1, out, src)
-}
-
 func NewStringWithLatin1Chars(content string) String {
 	cx := String{}
 	ptr := (GDExtensionUninitializedStringPtr)(cx.NativePtr())

test/pkg/example.go
@@ -701,7 +701,7 @@ func GetExamplePropertyList() ([]GDExtensionPropertyInfo, func()) {
 			pn.Destroy()
 			hs.Destroy()
 		})
-		return NewGDExtensionPropertyInfoFromNames(&cn, vt, &pn, uint32(PROPERTY_HINT_NONE), &hs, uint32(PROPERTY_USAGE_DEFAULT))
+		return NewGDExtensionPropertyInfoFromNames(&cn, vt, &pn, &hs)
 	}
 
 	props[0] = makeProp("Example", "property_from_list", GDEXTENSION_VARIANT_TYPE_VECTOR3)
@@ -721,6 +721,7 @@ func RegisterClassExample() {
 	props, cleanup := GetExamplePropertyList()
 	defer cleanup()
 	ClassDBRegisterClass(NewExampleFromOwnerObject, props, ValidateExampleProperty, func(t *Example) {
+		// virtuals
 		// virtuals
 		ClassDBBindMethodVirtual(t, "V_ToString", "to_string", nil, nil)
 		ClassDBBindMethodVirtual(t, "V_Ready", "_ready", nil, nil)

test/pkg/lib.go
@@ -10,6 +10,7 @@ import (
 
 	. "github.com/godot-go/godot-go/pkg/core"
 	"github.com/godot-go/godot-go/pkg/ffi"
+	. "github.com/godot-go/godot-go/pkg/gdclassimpl"
 	"github.com/godot-go/godot-go/pkg/log"
 	"github.com/godot-go/godot-go/pkg/util"
 )
@@ -23,6 +24,7 @@ func RegisterExampleTypes() {
 func UnregisterExampleTypes() {
 	log.Debug("UnregisterExampleTypes called")
 	UnregisterClassExample()
+	DestroyAllCachedClassNames()
 }
 
 //export TestDemoInit

commit d7ed1d27639edec687239d9b2fee538e5ece8dc5

fix: restore StringName/NodePath lifecycle management (#136)
* fix: restore StringName/NodePath lifecycle management

- Eliminate orphan warnings on make test.
- Restore CopyConstructor + Destroy in return encoding paths
  (GDExtensionTypePtrFromReflectValue ptrcall branches,
  GDExtensionVariantPtrFromReflectValue varcall branch)
- Revert argument decoding to borrow semantics:
  varcall: ToStringName + byte-copy + Destroy,
  ptrcall: raw byte-copy of const-type pointer
- Fixes dormant regressions from b4a0880 (return path revert)
  and 6450f9c (arg decode ownership change)
- make test: 595/595 pass, 0 orphan warnings, no corruption at exit

* chore: archive fix-orphan-stringname-warnings change and sync main spec

pkg/core/method_bind_reflect.go
@@ -315,10 +315,20 @@ func convertVariantToGoTypeReflectValue(arg Variant, t reflect.Type) (reflect.Va
 			return reflect.ValueOf(v), nil
 		case StringName:
 			v := arg.ToStringName()
-			return reflect.ValueOf(v), nil
+			var borrow StringName
+			for i := 0; i < StringNameSize; i++ {
+				borrow[i] = v[i]
+			}
+			v.Destroy()
+			return reflect.ValueOf(borrow), nil
 		case NodePath:
 			v := arg.ToNodePath()
-			return reflect.ValueOf(v), nil
+			var borrow NodePath
+			for i := 0; i < NodePathSize; i++ {
+				borrow[i] = v[i]
+			}
+			v.Destroy()
+			return reflect.ValueOf(borrow), nil
 		default:
 			log.Panic("unsupported array type",
 				zap.Any("type", t),
@@ -765,12 +775,10 @@ func reflectFuncCallArgsFromGDExtensionConstTypePtrSliceArgs(inst GDClass, suppl
 				v := *(*String)(arg)
 				args[i+1] = reflect.ValueOf(v)
 			case StringName:
-				var v StringName
-				StringNameCopyConstructor((GDExtensionUninitializedTypePtr)(&v), (GDExtensionConstTypePtr)(arg))
+				v := NewStringNameWithGDExtensionConstStringNamePtr((GDExtensionConstStringNamePtr)(arg))
 				args[i+1] = reflect.ValueOf(v)
 			case NodePath:
-				var v NodePath
-				NodePathCopyConstructor((GDExtensionUninitializedTypePtr)(&v), (GDExtensionConstTypePtr)(arg))
+				v := *(*NodePath)(arg)
 				args[i+1] = reflect.ValueOf(v)
 			default:
 				log.Panic(fmt.Sprintf("MethodBind.Ptrcall reflected as array does not support type: %s", t.Name()))

pkg/core/variant_refect_value.go
@@ -105,9 +105,11 @@ func GDExtensionTypePtrFromReflectValue(value reflect.Value, rOut GDExtensionUni
 		case Color:
 			ColorEncoder.EncodeTypePtrArg(inst, rOut)
 		case StringName:
-			StringNameEncoder.EncodeTypePtrArg(inst, rOut)
+			StringNameCopyConstructor(rOut, inst.NativeConstPtr())
+			inst.Destroy()
 		case NodePath:
-			NodePathEncoder.EncodeTypePtrArg(inst, rOut)
+			NodePathCopyConstructor(rOut, inst.NativeConstPtr())
+			inst.Destroy()
 		case RID:
 			RIDEncoder.EncodeTypePtrArg(inst, rOut)
 		case Callable:
@@ -191,9 +193,11 @@ func GDExtensionTypePtrFromReflectValue(value reflect.Value, rOut GDExtensionUni
 		case Color:
 			ColorEncoder.EncodeTypePtrArg(inst, rOut)
 		case StringName:
-			StringNameEncoder.EncodeTypePtrArg(inst, rOut)
+			StringNameCopyConstructor(rOut, inst.NativeConstPtr())
+			inst.Destroy()
 		case NodePath:
-			NodePathEncoder.EncodeTypePtrArg(inst, rOut)
+			NodePathCopyConstructor(rOut, inst.NativeConstPtr())
+			inst.Destroy()
 		case RID:
 			RIDEncoder.EncodeTypePtrArg(inst, rOut)
 		case Callable:
@@ -332,8 +336,10 @@ func GDExtensionVariantPtrFromReflectValue(value reflect.Value, rOut GDExtension
 			ColorEncoder.EncodeVariantPtrArg(inst, rOut)
 		case StringName:
 			StringNameEncoder.EncodeVariantPtrArg(inst, rOut)
+			inst.Destroy()
 		case NodePath:
 			NodePathEncoder.EncodeVariantPtrArg(inst, rOut)
+			inst.Destroy()
 		case RID:
 			RIDEncoder.EncodeVariantPtrArg(inst, rOut)
 		case Callable: